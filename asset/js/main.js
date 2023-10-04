af6498392a52938074218cbd5aefe3951a486791
dku124 <duongkhanhuyen124@gmail.com>
2023-10-04 22:57:42 +0700

    add scroll section

--- asset/js/main.js
+++ asset/js/main.js
@@ -18,11 +18,13 @@ const scrollActive = () =>{
 			  sectionTop = current.offsetTop - 58,
 			  sectionId = current.getAttribute('id'),
 			  sectionsClass = document.querySelector('.nav_item a[href*=' + sectionId + ']')
-
+        scrollClass = document.querySelector('.scroll_item a[href*=' + sectionId + ']')
 		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
 			sectionsClass.classList.add('active_link')
+      scrollClass.classList.add('active')
 		}else{
 			sectionsClass.classList.remove('active_link')
+			scrollClass.classList.remove('active')
 		}                                                    
 	})
 }
